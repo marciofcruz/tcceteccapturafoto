b2d7ba3626abdc79a237cb802f3c7dcd067568a6
Marcio Cruz <programador@marciofcruz.com>
2025-08-03 12:42:29 -0300

    ajustes debug

--- app_cadastro.py
+++ app_cadastro.py
@@ -318,4 +318,5 @@ def delete_photo(filename):
         return jsonify({'error': 'Arquivo não encontrado'}), 404
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    #app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(debug=False)
